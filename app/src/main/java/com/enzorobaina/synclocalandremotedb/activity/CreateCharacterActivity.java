0e4c0d9614a0e27afb9289f36e6e600d3b442b19
EnzoRobaina <enzo.robaina@gmail.com>
2020-09-02 17:18:44 -0300

    Refatora activities p/ consumir viewModel

--- app/src/main/java/com/enzorobaina/synclocalandremotedb/activity/CreateCharacterActivity.java
+++ app/src/main/java/com/enzorobaina/synclocalandremotedb/activity/CreateCharacterActivity.java
@@ -1,20 +1,21 @@
 package com.enzorobaina.synclocalandremotedb.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputType;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
 import com.enzorobaina.synclocalandremotedb.R;
-import com.enzorobaina.synclocalandremotedb.api.Syncer;
-import com.enzorobaina.synclocalandremotedb.api.VoidCallback;
+import com.enzorobaina.synclocalandremotedb.api.LongCallback;
 import com.enzorobaina.synclocalandremotedb.database.ContentHelper;
 import com.enzorobaina.synclocalandremotedb.model.Character;
+import com.enzorobaina.synclocalandremotedb.model.CharacterViewModel;
 import com.enzorobaina.synclocalandremotedb.utils.ViewUtils;
 
 import java.util.Arrays;
@@ -30,6 +31,7 @@ public class CreateCharacterActivity extends AppCompatActivity {
     EditText intelligenceEditText;
     EditText wisdomEditText;
     EditText charismaEditText;
+    CharacterViewModel characterViewModel;
     Button submit;
     View spinner;
 
@@ -51,26 +53,19 @@ public class CreateCharacterActivity extends AppCompatActivity {
         submit = findViewById(R.id.btnSubmit);
         contentHelper = ContentHelper.getInstance(this);
         spinner = findViewById(R.id.spinnerOverlay);
+        characterViewModel = new ViewModelProvider(this).get(CharacterViewModel.class);
     }
 
-    private String _s(EditText editText){
-        return editText.getText().toString();
-    }
-
-    private int _i(EditText editText){
-        return Integer.parseInt(editText.getText().toString());
-    }
-
-    private void _showSpinner(){
+    private void showSpinner(){
         spinner.bringToFront();
         ViewUtils.animateView(spinner, View.VISIBLE, 0.4f, 200);
     }
 
-    private void _hideSpinner(){
+    private void hideSpinner(){
         ViewUtils.animateView(spinner, View.GONE, 0, 200);
     }
 
-    private boolean _fieldsAreOk(){
+    private boolean fieldsAreOk(){
         List<EditText> fields = Arrays.asList(nameEditText, strengthEditText, dexterityEditText, constitutionEditText, intelligenceEditText, wisdomEditText, charismaEditText);
         HashMap<String, String> fieldErrors = new HashMap<String, String>(){
             {
@@ -84,7 +79,7 @@ public class CreateCharacterActivity extends AppCompatActivity {
 
         for (EditText field : fields){
             field.setError(null);
-            if (_s(field).isEmpty()){
+            if (ViewUtils.getString(field).isEmpty()){
                 field.setError(fieldErrors.get("empty"));
                 isOk = false;
                 if (toBeFocused == null){
@@ -92,7 +87,7 @@ public class CreateCharacterActivity extends AppCompatActivity {
                 }
             }
             if (field.getInputType() == InputType.TYPE_CLASS_NUMBER){
-                int intValue = _i(field);
+                int intValue = ViewUtils.getInt(field);
                 if (intValue < 1 || intValue > 20){
                     field.setError(fieldErrors.get("invalid-numeric"));
                     isOk = false;
@@ -108,14 +103,14 @@ public class CreateCharacterActivity extends AppCompatActivity {
         return isOk;
     }
 
-    private void _disableSubmitBtn(){
+    private void disableSubmitBtn(){
         if (submit == null){
             return;
         }
         submit.setEnabled(false);
     }
 
-    private void _enableSubmitBtn(){
+    private void enableSubmitBtn(){
         if (submit == null){
             return;
         }
@@ -123,51 +118,42 @@ public class CreateCharacterActivity extends AppCompatActivity {
     }
 
     public void onSubmit(View view){
-        if (!_fieldsAreOk()){
+        if (!fieldsAreOk()){
             return;
         }
 
-        _disableSubmitBtn();
-        _showSpinner();
+        disableSubmitBtn();
+        showSpinner();
+
         Character character = new Character();
-        character.setName(_s(nameEditText));
-        character.setStrength(_i(strengthEditText));
-        character.setDexterity(_i(dexterityEditText));
-        character.setConstitution(_i(constitutionEditText));
-        character.setIntelligence(_i(intelligenceEditText));
-        character.setWisdom(_i(wisdomEditText));
-        character.setCharisma(_i(charismaEditText));
+        character.setName(ViewUtils.getString(nameEditText));
+        character.setStrength(ViewUtils.getInt(strengthEditText));
+        character.setDexterity(ViewUtils.getInt(dexterityEditText));
+        character.setConstitution(ViewUtils.getInt(constitutionEditText));
+        character.setIntelligence(ViewUtils.getInt(intelligenceEditText));
+        character.setWisdom(ViewUtils.getInt(wisdomEditText));
+        character.setCharisma(ViewUtils.getInt(charismaEditText));
 
-        long recentlyCreatedCharacterId = contentHelper.createCharacter(character);
+        characterViewModel.insert(character, new LongCallback() {
+            @Override
+            public void onSuccess(long value) {
+                runOnUiThread(() -> {
+                    Toast.makeText(getApplicationContext(), String.valueOf(value), Toast.LENGTH_LONG).show();
+                    finish();
+                });
+            }
 
-        if (recentlyCreatedCharacterId > 0){
-            character.setId((int) recentlyCreatedCharacterId); // TODO: Figure a better way to set the id
-            Syncer syncer = Syncer.getInstance(this);
-            syncer.syncOne(character, new VoidCallback() {
-                @Override
-                public void onSuccess() {
-                    Toast.makeText(getApplicationContext(), "Sync One Done!", Toast.LENGTH_LONG).show();
-                }
-
-                @Override
-                public void onFail() {
-                    Toast.makeText(getApplicationContext(), "Sync One Failed!", Toast.LENGTH_LONG).show();
-                }
-
-                @Override
-                public void always() {
-                    _hideSpinner();
-                    _startListActivity();
-                }
-            });
-        }
-        else {
-            Log.d("recentlyCreatedCharId", "< 0");
-            _enableSubmitBtn();
-        }
+            @Override
+            public void onFail() {
+                runOnUiThread(() -> {
+                    hideSpinner();
+                    enableSubmitBtn();
+                });
+            }
+        });
     }
 
-    private void _startListActivity(){
+    private void startListActivity(){
         startActivity(new Intent(this, ListCharacterActivity.class));
     }
 }
--- app/src/main/java/com/enzorobaina/synclocalandremotedb/activity/ListCharacterActivity.java
+++ app/src/main/java/com/enzorobaina/synclocalandremotedb/activity/ListCharacterActivity.java
@@ -1,114 +1,41 @@
 package com.enzorobaina.synclocalandremotedb.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 import com.enzorobaina.synclocalandremotedb.R;
-import com.enzorobaina.synclocalandremotedb.adapter.CharacterAdapter;
-import com.enzorobaina.synclocalandremotedb.api.Syncer;
-import com.enzorobaina.synclocalandremotedb.api.VoidCallback;
-import com.enzorobaina.synclocalandremotedb.database.ContentHelper;
-import com.enzorobaina.synclocalandremotedb.utils.ViewUtils;
+import com.enzorobaina.synclocalandremotedb.adapter.CharacterViewModelAdapter;
+import com.enzorobaina.synclocalandremotedb.model.CharacterViewModel;
 
 public class ListCharacterActivity extends AppCompatActivity {
-    RecyclerView recyclerView;
-    ContentHelper contentHelper;
-    View spinner;
+    private RecyclerView recyclerView;
+    private CharacterViewModelAdapter adapter;
+    private CharacterViewModel characterViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_character2);
-        initComponents();
-    }
-
-    private void initComponents(){
-        spinner = findViewById(R.id.spinnerOverlay);
-        contentHelper = ContentHelper.getInstance(this);
-
         recyclerView = findViewById(R.id.listCharacterRecyclerView);
+        adapter = new CharacterViewModelAdapter(this);
+
+        recyclerView.setAdapter(adapter);
         recyclerView.setHasFixedSize(true);
         recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayout.VERTICAL));
-
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        Syncer syncer = Syncer.getInstance(this);
-        syncer.runFirst(new VoidCallback() {
-            @Override
-            public void onSuccess() {
-                Toast.makeText(getApplicationContext(), "First Sync Done!", Toast.LENGTH_LONG).show(); // TODO: Handle this
-            }
+        characterViewModel = new ViewModelProvider(this).get(CharacterViewModel.class);
 
-            @Override
-            public void onFail() {
-                Toast.makeText(getApplicationContext(), "Something Went Wrong", Toast.LENGTH_LONG).show(); // TODO: Handle this
-            }
-
-            @Override
-            public void always() {
-                Log.d("runFirst", "always");
-                fillRecycler();
-            }
-        });
+        characterViewModel.getAllCharacters().observe(this, characters -> adapter.setCharacters(characters));
     }
 
-    private void fillRecycler(){
-        if (recyclerView.getAdapter() == null){
-            recyclerView.setAdapter(new CharacterAdapter(contentHelper.getAllCharacters()));
-        }
-        recyclerView.getAdapter().notifyDataSetChanged();
-    }
-
-    public void addCharacter(View view){
+    public void addCharacter(View view) {
         startActivity(new Intent(this, CreateCharacterActivity.class));
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        this.fillRecycler();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        this.fillRecycler();
-    }
-
-    private void _showSpinner(){
-        spinner.bringToFront();
-        ViewUtils.animateView(spinner, View.VISIBLE, 0.4f, 200);
-    }
-
-    private void _hideSpinner(){
-        ViewUtils.animateView(spinner, View.GONE, 0, 200);
-    }
-
-    public void syncCharacters(View view){
-        _showSpinner();
-        Syncer.getInstance(this).syncRemoteWithLocal(new VoidCallback() {
-            @Override
-            public void onSuccess() {
-                Toast.makeText(getApplicationContext(), "Sync Done!", Toast.LENGTH_LONG).show(); // TODO: Handle this
-            }
-
-            @Override
-            public void onFail() {
-                Toast.makeText(getApplicationContext(), "Error Syncing", Toast.LENGTH_LONG).show();
-            }
-
-            @Override
-            public void always() {
-                _hideSpinner();
-                fillRecycler();
-            }
-        });
     }
 }
