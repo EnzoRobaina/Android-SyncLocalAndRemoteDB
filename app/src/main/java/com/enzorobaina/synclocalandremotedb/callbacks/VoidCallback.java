b077685237cd1d1f22666ad5e8417e9669936ab3
EnzoRobaina <enzo.robaina@gmail.com>
2020-09-09 12:57:02 -0300

    Torna VoidCallback extensão de VoidCallback1

--- app/src/main/java/com/enzorobaina/synclocalandremotedb/callbacks/VoidCallback.java
+++ app/src/main/java/com/enzorobaina/synclocalandremotedb/callbacks/VoidCallback.java
@@ -1,7 +1,6 @@
-package com.enzorobaina.synclocalandremotedb.api;
+package com.enzorobaina.synclocalandremotedb.callbacks;
 
-public interface VoidCallback {
-    void onSuccess();
+public interface VoidCallback extends VoidCallback1 {
     void onFail();
     void always();
 }
