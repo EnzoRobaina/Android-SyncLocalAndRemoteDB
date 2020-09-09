927f75f0d1e4a1598b392b4b55cc97945802f871
EnzoRobaina <enzo.robaina@gmail.com>
2020-09-09 12:53:44 -0300

    Adiciona fallbackToDestructive ao migrar db

--- app/src/main/java/com/enzorobaina/synclocalandremotedb/database/CharacterRoomDatabase.java
+++ app/src/main/java/com/enzorobaina/synclocalandremotedb/database/CharacterRoomDatabase.java
@@ -5,24 +5,24 @@ import android.content.Context;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
-
 import com.enzorobaina.synclocalandremotedb.model.Character;
-
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-@Database(entities = {Character.class}, version = 2, exportSchema = false)
+@Database(entities = {Character.class}, version = 1, exportSchema = false)
 public abstract class CharacterRoomDatabase extends RoomDatabase {
     public static final String DATABASE_NAME = "character_database";
     public abstract CharacterDAO characterDAO();
     private static volatile CharacterRoomDatabase INSTANCE;
-    public static final ExecutorService databaseWriteExecutor = Executors.newSingleThreadExecutor();
+    public static final ExecutorService databaseWriteExecutor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
     public static CharacterRoomDatabase getInstance(final Context context){
         if (INSTANCE == null){
             synchronized (CharacterRoomDatabase.class){
                 if (INSTANCE == null){
-                    INSTANCE = Room.databaseBuilder(context, CharacterRoomDatabase.class, DATABASE_NAME).build();
+                    INSTANCE = Room.databaseBuilder(context, CharacterRoomDatabase.class, DATABASE_NAME)
+                            .fallbackToDestructiveMigration()
+                            .build();
                 }
             }
         }
