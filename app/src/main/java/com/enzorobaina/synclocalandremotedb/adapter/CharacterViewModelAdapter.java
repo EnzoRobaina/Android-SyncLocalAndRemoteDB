e00894a1920af09e0bf5abe9dc27c6847e123579
EnzoRobaina <enzo.robaina@gmail.com>
2020-09-09 18:10:34 -0300

    Adiciona novos TextViews ao adapter de Character

--- app/src/main/java/com/enzorobaina/synclocalandremotedb/adapter/CharacterViewModelAdapter.java
+++ app/src/main/java/com/enzorobaina/synclocalandremotedb/adapter/CharacterViewModelAdapter.java
@@ -1,19 +1,16 @@
 package com.enzorobaina.synclocalandremotedb.adapter;
 
-import android.content.Context;
-import android.util.Log;
+import android.annotation.SuppressLint;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.enzorobaina.synclocalandremotedb.R;
 import com.enzorobaina.synclocalandremotedb.model.Character;
-
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 public class CharacterViewModelAdapter extends RecyclerView.Adapter<CharacterViewModelAdapter.CharacterViewHolder>{
@@ -21,19 +18,22 @@ public class CharacterViewModelAdapter extends RecyclerView.Adapter<CharacterVie
         TextView nameTextView;
         TextView idTextView;
         ImageView isSyncedImageView;
+        TextView uuidTextView;
+        TextView lastModifiedAtTextView;
 
         CharacterViewHolder(@NonNull View itemView) {
             super(itemView);
             nameTextView = itemView.findViewById(R.id.nameTextView);
             idTextView = itemView.findViewById(R.id.idTextView);
             isSyncedImageView = itemView.findViewById(R.id.isSyncedImageView);
+            uuidTextView = itemView.findViewById(R.id.uuidTextView);
+            lastModifiedAtTextView = itemView.findViewById(R.id.lastModifiedAtTextView);
         }
     }
 
-    private final LayoutInflater inflater;
     private List<Character> characters;
 
-    public CharacterViewModelAdapter(Context context) { inflater = LayoutInflater.from(context); }
+    public CharacterViewModelAdapter() {}
 
     @NonNull
     @Override
@@ -41,12 +41,22 @@ public class CharacterViewModelAdapter extends RecyclerView.Adapter<CharacterVie
         return new CharacterViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_character, parent, false));
     }
 
+    @SuppressLint({"SimpleDateFormat", "SetTextI18n"})
     @Override
     public void onBindViewHolder(CharacterViewHolder holder, int position) {
         if (characters != null) {
             Character current = characters.get(position);
             holder.nameTextView.setText(current.getName());
             holder.idTextView.setText(String.valueOf(current.getId()));
+            holder.lastModifiedAtTextView.setText(
+                    new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(current.getLastModifiedAt())
+            );
+            if (current.getUuid() != null){
+                holder.uuidTextView.setText(current.getUuid().isEmpty() ? "TBD" : current.getUuid());
+            }
+            else {
+                holder.uuidTextView.setText("TBD");
+            }
             if (current.isSynced()){
                 holder.isSyncedImageView.setImageResource(android.R.drawable.presence_online);
             }
